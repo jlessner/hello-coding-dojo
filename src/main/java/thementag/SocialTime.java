e05380813705966d3285e6eb1b8ba30b87b792e9
Jan Lessner <jan.lessner@sn-invent.de>
2025-06-13 09:58:37 +0200

    Task 1 -Test added

--- src/main/java/thementag/SocialTime.java
+++ src/main/java/thementag/SocialTime.java
@@ -3,6 +3,9 @@ package thementag;
 public class SocialTime {
 
   public String socialTime(int ageInSeconds) {
+    if (ageInSeconds < 0) {
+      throw new IllegalArgumentException("Age must be a positive integer");
+    }
     if (ageInSeconds < 10) {
       return "few seconds";
     }
--- src/test/java/thementag/SocialTimeTest.java
+++ src/test/java/thementag/SocialTimeTest.java
@@ -1,20 +1,44 @@
 package thementag;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SocialTimeTest {
 
-  @Test
-  void testFewSeconds() {
-    String time = new SocialTime().socialTime(5);
+  @ParameterizedTest
+  @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+  void testFewSeconds(int seconds) {
+    String time = new SocialTime().socialTime(seconds);
     assertEquals("few seconds", time);
   }
 
+  @ParameterizedTest
+  @CsvSource({
+      "10, 10 seconds",
+      "11, 11 seconds",
+      "12, 12 seconds",
+      "59, 59 seconds",
+      "60, 60 seconds",
+      "120, 120 seconds",
+      "3600, 3600 seconds"
+  })
+  void testSeconds(int seconds, String expected) {
+    String time = new SocialTime().socialTime(seconds);
+    assertEquals(expected, time);
+  }
+
   @Test
-  void test11Seconds() {
-    String time = new SocialTime().socialTime(11);
-    assertEquals("11 seconds", time);
+  void testNegativeSeconds() {
+    boolean exception = false;
+    try {
+      new SocialTime().socialTime(-11);
+    } catch (IllegalArgumentException e) {
+      exception = true;
+    }
+    assertTrue(exception);
   }
 }
