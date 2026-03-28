96790bb27ddb5273a57c262d328d560d0b150c6a
Jan Lessner <jan.lessner@sn-invent.de>
2026-03-28 18:52:56 +0100

    06-Zurück-Auf-Null

--- src/main/java/two_word_anagrams/Wort.java
+++
@@ -1,9 +0,0 @@
-package two_word_anagrams;
-
-public record Wort(String text) {
-
-    @Override
-    public String toString() {
-        return text;
-    }
-}
--- src/main/java/two_word_anagrams/WortPaar.java
+++
@@ -1,9 +0,0 @@
-package two_word_anagrams;
-
-public record WortPaar(Wort erstesWort, Wort zweitesWort) {
-
-    @Override
-    public String toString() {
-        return erstesWort + " + " + zweitesWort;
-    }
-}
